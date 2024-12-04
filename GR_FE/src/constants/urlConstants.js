27e8a9625de1f74dd2b80242299086dd82f06d37
TRAN Minh Huyen <141987670+DI-huyentm@users.noreply.github.com>
2024-12-04 14:10:46 +0700

    Update server urlConstants.js

--- GR_FE/src/constants/urlConstants.js
+++ GR_FE/src/constants/urlConstants.js
@@ -1,5 +1,5 @@
-// export const BASE_URL = "http://52.76.180.180:3001/api";
-export const BASE_URL = "http://localhost:3001/api";
+export const BASE_URL = "http://74.176.104.55:3001/api";
+// export const BASE_URL = "http://localhost:3001/api";
 
-// export const SOCKET_SERVER_URL = "http://52.76.180.180:3001";
-export const SOCKET_SERVER_URL = "http://localhost:3001";
+export const SOCKET_SERVER_URL = "http://74.176.104.55:3001";
+// export const SOCKET_SERVER_URL = "http://localhost:3001";
